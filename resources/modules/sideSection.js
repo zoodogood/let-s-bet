c26cb756d9cb2bbdb3bc5a67cfec24550b371e6c
zoodogood <52154209+zoodogood@users.noreply.github.com>
2022-03-03 12:15:03 +0200

    Add files via upload

---
+++ resources/modules/sideSection.js
@@ -0,0 +1,47 @@
+class MainElement {
+
+}
+
+
+class LeaderBoard {
+  constructor(game){
+    this.game = game;
+  }
+
+  setHandlers(){
+    game.on("fail", async ({ game }) => {
+      const options = {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "text/plain"
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+      }
+
+      fetch(`https://stats-co.zoodogood.repl.co/let-s-bet/score/>100?method=increment`, options);
+    })
+
+    game.on("victory", async ({ game, score }) => {
+      const options = {
+        method: "POST",
+        mode: "cors",
+        cache: "no-cache",
+        credentials: "same-origin",
+        headers: {
+          "Content-Type": "text/plain"
+        },
+        redirect: "follow",
+        referrerPolicy: "no-referrer",
+      }
+
+      fetch(`https://stats-co.zoodogood.repl.co/let-s-bet/score/=${ score }?method=increment`, options);
+    })
+  }
+}
+
+
+new LeaderBoard(globalThis.game);
